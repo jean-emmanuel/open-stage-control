53f941c1e764053111d8406b671a48d2aa88f9ec
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2025-09-18 00:23:12 +0200

    packaging: update electron packager, it doesn't require wine anymore to build win pacakge from linux

--- scripts/package.js
+++ scripts/package.js
@@ -1,4 +1,4 @@
-const packager = require('electron-packager'),
+const packager = require('@electron/packager'),
       path = require('path'),
       appData = require('../app/package.json'),
       safeFFMPEG = require('electron-packager-plugin-non-proprietary-codecs-ffmpeg').default,
